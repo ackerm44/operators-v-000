adec75942cd5d48e7f6dd46a3dbbd3e6ec14ebfb
Amanda Ackerman <ackerm44@gmail.com>
2017-10-20 15:51:25 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 def unsafe?(speed)
  if speed > 60 || speed < 40
-	 true
+	 return true
  else
-	 false
+	 return false
 end
 
 
@@ -10,6 +10,3 @@ end
 def not_safe?(speed)
 	speed > 60 || speed < 40 ? true : false
 end
-	
-
-
